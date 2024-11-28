fcce2cba0cccfc371d6613ffc06278266bf62ff1
Mark Vodyanitskiy <mvodya@icloud.com>
2024-11-28 23:46:40 +1000

    Main file template

--- ts/bin/mandelbulb_singlethread.ts
+++ ts/bin/mandelbulb_singlethread.ts
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log("TODO")
+// Image width
+const WIDTH = 2048;
+// Image height
+const HEIGHT = 2048;
+
+// Number of renders to be performed (identical renders for quality check)
+const STEPS = 10;
+
+//////
+
+(async () => {
+  console.log("[TS] Mandelbulb single thread benchmark");
+  console.log("TODO");
+
+  // Steps
+  for (let i = 0; i < STEPS; i++) {}
+})();
