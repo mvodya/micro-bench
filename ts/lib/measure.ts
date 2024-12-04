12d4034d30c5196f1cd06386559d2f8891dd8944
Mark Vodyanitskiy <mvodya@icloud.com>
2024-12-04 22:22:03 +1000

    Fixed missed code & test

--- ts/lib/measure.ts
+++ ts/lib/measure.ts
@@ -1,18 +1,20 @@
+type AnyFunction = (...args: any[]) => any;
+
 // Measure function execution time
-export const measure = async (func, ...args) => {
+export const measure = async <T>(func: AnyFunction, ...args: Parameters<typeof func>) => {
   // Start time
   const start = getCurrentTime();
 
   // Check type of function
   if (func.constructor.name === "AsyncFunction") {
     // Async function execution
-    const result = await func(...args);
+    const result = await func(...args) as T;
     // End time
     const end = getCurrentTime();
     return { result, duration: Number(end - start) };
   } else {
     // Sync function execution
-    const result = func(...args);
+    const result = func(...args) as T;
     // End time
     const end = getCurrentTime();
     return { result, duration: Number(end - start) };
