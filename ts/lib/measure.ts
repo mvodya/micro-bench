5a3e563018af2d57cef486239f96934ff6cb903a
Mark Vodyanitskiy <mvodya@icloud.com>
2024-12-04 22:22:23 +1000

    Autoformat

--- ts/lib/measure.ts
+++ ts/lib/measure.ts
@@ -1,14 +1,17 @@
 type AnyFunction = (...args: any[]) => any;
 
 // Measure function execution time
-export const measure = async <T>(func: AnyFunction, ...args: Parameters<typeof func>) => {
+export const measure = async <T>(
+  func: AnyFunction,
+  ...args: Parameters<typeof func>
+) => {
   // Start time
   const start = getCurrentTime();
 
   // Check type of function
   if (func.constructor.name === "AsyncFunction") {
     // Async function execution
-    const result = await func(...args) as T;
+    const result = (await func(...args)) as T;
     // End time
     const end = getCurrentTime();
     return { result, duration: Number(end - start) };
