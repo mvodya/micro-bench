0ec84f4640cd1bbb12c12552536754e7a2d7d0d8
Mark Vodyanitskiy <mvodya@icloud.com>
2024-12-04 23:45:48 +1000

    Auto format

--- ts/lib/index.ts
+++ ts/lib/index.ts
@@ -1,5 +1,5 @@
 import Vec3 from "./vec3";
 import { measure } from "./measure";
-import * as mandelbulblib from "./mandelbulblib"
+import * as mandelbulblib from "./mandelbulblib";
 
 export { Vec3, measure, mandelbulblib };
